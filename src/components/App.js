f200c7ae1c1f64d69b65b8dc8ab3d5cc534c2f98
mythLabs <amitbisht744@gmail.com>
2018-11-19 23:00:32 +0530

    Base setup

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import SearchBar from './SearchBar';
+
+
+const App = ()=>{
+    return (
+    <SearchBar/>
+    )
+}
+
+
+export default App;
---
+++ src/components/SearchBar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class SearchBar extends React.Component{
+    render() {
+        return (
+            <div>
+                <form>
+                    <input ></input>
+                </form>
+            </div>
+        )
+    }
+}
+
+
+export default SearchBar;
